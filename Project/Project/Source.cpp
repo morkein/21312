d14e07e85398ddb563b839b4d617c657eaf351fa
StahievichLiza <liza.12321@mail.ru>
2020-12-20 14:31:36 +0300

    2.1 added Multiplication

--- Project/Project/Source.cpp
+++ Project/Project/Source.cpp
@@ -1,7 +1,8 @@
-//version 2.0 ready to release
+
 
 #include "feature1.h"
 #include "feature2.h"
+#include "feature3.h"
 #include <iostream>
 using namespace std;
 
@@ -33,7 +34,7 @@ int main()
 		cout << a << " - " << b << " = " << Sub(a, b) << endl; //Вычитание
 		break; 
 	case 3:	
-		cout << "Функция в разработке" << endl; //Умножение
+		cout << a << " * " << b << " = " << Mult(a, b) << endl; //Умножение
 		break;
 	case 4: 
 		cout << "Функция в разработке" << endl; //Деление
